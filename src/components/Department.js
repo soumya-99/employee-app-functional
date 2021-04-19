1ae426eeea7f95da444b91298725dade4c7b8cba
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2021-04-20 00:26:36 +0530

    [0.2.0] Added adding feature (trying to fix a minor bug)

--- src/components/Department.js
+++ src/components/Department.js
@@ -5,6 +5,7 @@ const API_ENDPOINT = "http://127.0.0.1:8000/"
 
 function Department() {
 	const [departments, setDepartments] = useState([])
+	const [inputField, setInputField] = useState("")
 	// const [addModalShow, setAddModalShow] = useState(false)
 	// const [editModalShow, setEditModalShow] = useState(false)
 
@@ -17,6 +18,7 @@ function Department() {
 		refreshList()
 	}, [])
 
+	// Deleting Department
 	const deleteDepartment = (departmentId) => {
 		const originalDepartments = [...departments]
 		const newArrayOfDepatments = departments.filter(
@@ -32,10 +34,75 @@ function Department() {
 		}
 	}
 
+	// Adding Departments
+	const addDepartment = async (e) => {
+		e.preventDefault()
+		const obj = { "DepartmentName": inputField }
+		const { data: department } = await axios.post(
+			`${API_ENDPOINT}department`,
+			obj
+		)
+		const newArrayOfDepartments = [department, ...departments]
+		setDepartments(newArrayOfDepartments)
+		setInputField("")
+	}
+
 	return (
 		<div>
 			<h1>Department</h1>
-			<table className="table table-striped table-bordered">
+			<hr />
+			<button
+				type="button"
+				className="btn btn-primary mb-2"
+				data-bs-toggle="modal"
+				data-bs-target="#exampleModal"
+			>
+				+ Add Department
+			</button>
+
+			{/* Modal */}
+			<div
+				className="modal fade"
+				id="exampleModal"
+				aria-labelledby="exampleModalLabel"
+				aria-hidden="true"
+			>
+				<div className="modal-dialog">
+					<div className="modal-content">
+						<div className="modal-header">
+							<h5 className="modal-title" id="exampleModalLabel">
+								Adding Department
+							</h5>
+						</div>
+						<div className="modal-body">
+							<form onSubmit={addDepartment}>
+								<input
+									className="form-control"
+									type="text"
+									placeholder="Department Name"
+									aria-label="default input example"
+									value={inputField}
+									onChange={(e) => setInputField(e.target.value)}
+								/>
+								<button type="submit" className="btn btn-success mt-5" disabled={!inputField}>
+									Save Changes
+								</button>
+							</form>
+						</div>
+						<div className="modal-footer">
+							<button
+								type="button"
+								className="btn btn-secondary"
+								data-bs-dismiss="modal"
+							>
+								Close
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<table className="table table-striped table-bordered table-hover">
 				<thead>
 					<tr>
 						<th>Department ID</th>
@@ -49,7 +116,7 @@ function Department() {
 							<td>{department.DepartmentID}</td>
 							<td>{department.DepartmentName}</td>
 							<td>
-								<button className="btn btn-primary mr-2">Edit</button>
+								<button className="btn btn-warning mr-2">Edit</button>
 								<button
 									className="btn btn-danger"
 									onClick={() => deleteDepartment(department)}
